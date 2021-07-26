59bd1d671f3a6465cba19c3690c71ed28e08d3d8
Sam <milk3531@hotmail.com>
2021-07-26 16:57:21 -0700

    basic html/css with display, but no calculator functionalities yet

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ function operate(firstNum, secondNum, operator) {
     if (operator === '+') {
         return firstNum + secondNum;
     } else if (operator === '-') {
-        return firstNum - secondNum 
+        return firstNum - secondNum
     } else if (operator === '*') {
         return firstNum * secondNum
     } else if (operator === '/') {
@@ -17,16 +17,31 @@ function operate(firstNum, secondNum, operator) {
 
 let display = document.querySelector('#screen')
 let buttons = document.querySelectorAll('div.grid-item')
+let numlist = [];
 
 
 
 
 // Display the corresponding number / operations
-for (let i = 0; i < buttons.length ; i++) {
-    buttons[i].addEventListener('click', function(){
-        display.innerHTML = buttons[i].innerHTML
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        if (buttons[i].innerHTML === 'AC') {
+            display.innerHTML = '';
+            numlist = [];
+        }
+        else if (buttons[i].innerHTML === 'DEL') {
+            // get rid of last number
+        }
+        else {
+            // probably split where operators are and add the two sums in the function operate
+            display.innerHTML += buttons[i].innerHTML
+            numlist.push(buttons[i].innerHTML);
+            console.log(numlist);
+        }
     })
 }
+
+
 
 
 
