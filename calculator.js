6d72bc77a42ce67c87c2e38235aad990b600ca1a
Sam <milk3531@hotmail.com>
2021-08-06 21:22:54 -0700

    FINALLY got some parts to work

--- calculator.js
+++ calculator.js
@@ -1,20 +1,16 @@
 
 
 function operate(firstNum, secondNum, operator) {
-    if (firstNum === undefined || secondNum === undefined) {
-
-    } else {
-        if (operator === '+') {
-            return parseInt(firstNum) + parseInt(secondNum);
-        } else if (operator === '-') {
-            return parseInt(firstNum) - parseInt(secondNum)
-        } else if (operator === '*') {
-            return firstNum * secondNum
-        } else if (operator === '/') {
-            if (secondNum !== 0) {
-                return firstNum / secondNum
-            } else return 'Cannot divide by a 0!';
-        }
+    if (operator === '+') {
+        solution = parseInt(firstNum) + parseInt(secondNum);
+    } else if (operator === '-') {
+        solution = parseInt(firstNum) - parseInt(secondNum)
+    } else if (operator === '*') {
+        solution = firstNum * secondNum
+    } else if (operator === '/') {
+        if (secondNum !== 0) {
+            solution = firstNum / secondNum
+        } else return 'Cannot divide by a 0!';
     }
 
 }
@@ -25,12 +21,13 @@ let buttons = document.querySelectorAll('div.grid-item')
 let numlist = [];
 let secondlist = []
 let operator = ['+', '-', '*', '/', '='];
-let first = '';
-let second = '';
+let firstnumber = '';
+let secondnumber = '';
 let opercount = 0;
-let calculate = false;
+let clicked = 0;
 let curoperator;
-let results;
+let solution = ''
+let savesolution = ''
 
 
 
@@ -38,42 +35,42 @@ let results;
 // Display the corresponding number / operations
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
-        if (buttons[i].innerHTML === 'AC') {
-            display.innerHTML = '';
-            numlist = [];
-            secondlist = [];
+        if (!operator.includes(buttons[i].innerHTML) && clicked === 0) {
+            if (savesolution === '') {
+                firstnumber += buttons[i].innerHTML
+            } else {
+                firstnumber = solution
+            }
         }
-        else if (buttons[i].innerHTML === 'DEL') {
-            // get rid of last number
-            numlist.splice(numlist.length - 1)
-            display.innerHTML = numlist.join('')
-        }
-        else if (operator.includes(buttons[i].innerHTML) && !calculate) {
-            calculate = true
+        if (operator.includes(buttons[i].innerHTML)) {
+            clicked++;
             curoperator = buttons[i].innerHTML
-
+            console.log(curoperator);
+        }
+        if (!operator.includes(buttons[i].innerHTML) && clicked === 1) {
+            secondnumber += buttons[i].innerHTML
+            console.log(secondnumber);
+            operate(firstnumber, secondnumber, curoperator)
+            console.log(solution);
 
         }
-        else if (!calculate) {
+        if (operator.includes(buttons[i].innerHTML) && clicked === 2) {
+            savesolution = solution
+            secondnumber = ''
+            console.log(solution);
+        }
+        if (!operator.includes(buttons[i].innerHTML) && clicked === 2) {
+            firstnumber = solution
+            secondnumber+= buttons[i].innerHTML
+            operate(firstnumber,secondnumber,curoperator)
+            console.log(firstnumber);
+            secondnumber = ''
+            console.log(solution);
+            clicked = 0
+        }
+        else {
             display.innerHTML += buttons[i].innerHTML
-            if (results === undefined) {
-                first += buttons[i].innerHTML
-            }
-        } else {
-            second += buttons[i].innerHTML // second is initialized as undefined
-            console.log(typeof parseInt(second));
-            console.log(operate(parseInt(first), parseInt(second), curoperator));
-            results = operate(parseInt(first), parseInt(second), curoperator)
-            if (operator.includes(buttons[i].innerHTML)) {
-                first += results // save latest result
-                curoperator = buttons[i].innerHTML
-                calculate = false
-                console.log(first)
-            }
-            // works the first time but not continous
         }
     })
 }
-// 3 * 2 display 6
-// 3 * 21 display 63
-// 
+
